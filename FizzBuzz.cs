b7c87729d71d30d38173fbca09ad0f50c89ef06b
stud <stud@W04N-P317D03>
2024-02-28 17:53:14 +0100

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKZad1
+{
+    internal class FizzBuzz
+    {
+        public int UpperBound { get; set; }
+        public FizzBuzz(int upperBound)
+        {
+            UpperBound = upperBound;
+        }
+        public void Display()
+        {
+            for(int i = 1; i <= UpperBound; i++)
+            {
+                if(i%3==0 && i%5==0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i%3==0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i%5==0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+               
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MKZad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz(60);
+            fizzBuzz.Display();
+        }
+    }
+}
